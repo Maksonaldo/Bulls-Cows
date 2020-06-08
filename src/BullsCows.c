2ff61b8ecff2a60496e8c67c254da0abb57c3413
Maksonaldo <shiliekov@mail.ru>
2020-06-08 19:05:32 +0700

    Compliance clang-format

--- src/BullsCows.c
+++ src/BullsCows.c
@@ -1,30 +1,27 @@
 #include "BullsCows"
 #include <stdio.h>
 
-void Hello(){
-	printf ("***************************\n");
-	printf ("***************************\n");
-	printf ("*******БЫКИ и КОРОВЫ*******\n");
-	printf ("***************************\n");
-	printf ("***************************\n");
-	printf ("***************************\n");
-	printf ("\n");
+void Hello() {
+  printf("***************************\n");
+  printf("***************************\n");
+  printf("*******БЫКИ и КОРОВЫ*******\n");
+  printf("***************************\n");
+  printf("***************************\n");
+  printf("***************************\n");
+  printf("\n");
 }
 
-void print_mass(char mass[], int n){
-	int i;
-	for (i=0; i<n; i++){
-    	printf("%c", mass[i]);
-    }
-
-
+void print_mass(char mass[], int n) {
+  int i;
+  for (i = 0; i < n; i++) {
+    printf("%c", mass[i]);
+  }
 }
 
-void rand_om(char mass[], int n){
-	srand(time(NULL));
-	int i;
-    for (i = 0; i < n; i++) {
-        mass[i] = rand() % 9 +'0';
- 
-    }
+void rand_om(char mass[], int n) {
+  srand(time(NULL));
+  int i;
+  for (i = 0; i < n; i++) {
+    mass[i] = rand() % 9 + '0';
+  }
 }
