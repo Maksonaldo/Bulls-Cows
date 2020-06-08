98e32d1f33cee91e670e82f920630a21f538621e
Maksonaldo <shiliekov@mail.ru>
2020-06-08 19:44:19 +0700

    Creating a recursive function location() that counts the number of cows and bulls

--- src/BullsCows.c
+++ src/BullsCows.c
@@ -1,4 +1,4 @@
-#include "BullsCows"
+#include "BullsCows.h"
 #include <stdio.h>
 bool no_same_digits(char mass[], int n){
 	int count=0;
@@ -12,4 +12,45 @@ bool no_same_digits(char mass[], int n){
 	}
 	//printf ("\n%d", count);
 	return (count != 0);
+}
+
+void location(char mass[], char mass2[], int n){
+	int bik=0;
+	int i,j, k=0;
+	int corova=0,hod=0;
+	char input;
+	
+	
+	printf ("Введите четыре символа которое вы считаете верным: ");
+	scanf("%s", mass2);
+	for (int i=0; i<mass2[i]; i++){
+		k++;//количество цифр в числе
+	}
+	//printf ("k = %d", k);
+	if (k==4){
+		
+	
+	for (i=0; i<n; i++){
+		for (j=0; j<n; j++){
+			if (mass[i]==mass2[j]){
+				if (i==j){
+					bik++;
+				}else{
+					corova++;
+				}
+
+			}
+
+		}
+
+	}
+	
+	printf ("\n");
+	printf ("****************************\n****bull = %d cow = %d ()****\n****************************\n", bik, corova); 
+	printf ("\n");
+}else{
+	printf("Вы должны ввести только 4 символа, попробуйте ещё раз\n");
+		hod++;
+		return location(mass,mass2,n);
+
 }
