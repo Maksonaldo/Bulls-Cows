7305b88e857e37fb372e926ad5b56f4f7c3488cd
Maksonaldo <shiliekov@mail.ru>
2020-08-27 21:32:08 +0700

    First stage of test development

--- test/test.c
+++ test/test.c
@@ -3,40 +3,66 @@
 
 
 
+int test_b(char mass[], char mass2[], int n) {
+  int bik = 0;
+  int i, j, k = 0;
+  int corova = 0;
+ 
+
+  
+  for (int i = 0; i < mass2[i]; i++) {
+    k++; //количество цифр в числе
+  }
+  // printf ("k = %d", k)s;
+  if (k == 4) {
+
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) {
+        if (mass[i] == mass2[j]) {
+          if (i == j) {
+            bik++;
+          } else {
+            corova++;
+          }
+        }
+        
+      }
+      
+    }
+    
+  }  
+  //return bik;
+  return bik;
+ 
+  } 
 
 
-CTEST(arithmetic_suite, simple_sum)
+CTEST(4_bulls, check)
 
 {
-	int n=4;
-	int b=4;
-//char mass[n];
-//char mass2[n];
-//rand_om(mass,n);
 
-ASSERT_EQUAL(n, b);
+int n = 4;
 
-
-
-/*
-    // Given
-
-    const int a = 1;
-
-    const int b = 2;
-
-
-
-    // When
-
-    const int result = a+b;
-
-
-
-    // Then
-
-    const int expected = 12;
-
-    ASSERT_EQUAL(expected, result);*/
-
+char mass[n];
+char mass2[n];
+mass[0]=1;
+mass[1]=2;
+mass[2]=3;
+mass[3]=4;
+mass2[0]=1;
+mass2[1]=2;
+mass2[2]=3;
+mass2[3]=4;
+//int a = b(mass,mass2,n);
+//printf("%d", a);
+for (int i = 0; i < n; ++i)
+{
+  //  printf("%d", mass[i]);
+}
+for (int i = 0; i < n; ++i)
+{
+   // printf("%d", mass2[j]);
+}
+int bulls=4;
+ASSERT_EQUAL(bulls,test_b(mass,mass2,n));
 }
